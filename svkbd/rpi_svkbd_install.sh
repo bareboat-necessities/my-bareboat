3757e7eee62ada27bae444cf8482793cd26829e5
mgrouch <mgrouch@users.noreply.github.com>
2020-01-16 17:17:42 -0500

    added work in progress (on screen keyboard and multitouch)

---
+++ svkbd/rpi_svkbd_install.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# See https://tools.suckless.org/x/svkbd/
+# and https://slackbuilds.org/repository/14.2/accessibility/svkbd/
+
+wget http://ponce.cc/slackware/sources/repo/svkbd-20140130.tar.gz
+gzip -cd svkbd-20140130.tar.gz | tar xvf -
+cd svkbd-20140130/
+export LAYOUT=en; make
+export LAYOUT=ru; make
+export LAYOUT=de; make
+export LAYOUT=arrows; make
+sudo cp svkbd-en /usr/bin/
+sudo cp svkbd-ru /usr/bin/
+sudo cp svkbd-de /usr/bin/
+sudo cp svkbd-arrows /usr/bin/
+sudo bash -c 'cat << EOF > /usr/bin/toggle-keyboard.sh
+#!/bin/bash
+PID=`pidof svkbd-en`
+if [ ! -e $PID ]; then
+  kill $PID
+else
+ svkbd-en -g 600x200+1+1 &
+fi
+EOF'
+sudo chmod +x /usr/bin/toggle-keyboard.sh
+sudo bash -c 'cat << EOF > /usr/share/raspi-ui-overrides/applications/toggle-keyboard.desktop
+[Desktop Entry]
+Name=Toggle Virtual Keyboard
+Comment=Toggle Virtual Keyboard
+Exec=/usr/bin/toggle-keyboard.sh
+Type=Application
+Icon=keyboard.png
+Categories=Panel;Utility
+EOF'
+sudo bash -c 'cat << EOF >> /home/pi/.config/lxpanel/LXDE-pi/panels/panel
+Plugin {
+  type=launchbar
+  Config {
+    Button {
+      id=toggle-keyboard.desktop
+    }
+  }
+}
+EOF'
+sudo reboot
---
+++ twofing/rpi_twofing_install.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# See  http://plippo.de/dev_twofing
+# and https://github.com/sjlongland/twofing
+
+sudo apt-get install build-essential libx11-dev libxtst-dev libxi-dev x11proto-randr-dev libxrandr-dev
+wget http://plippo.de/dwl/twofing/twofing-0.1.2.tar.gz
+tar -xvzf twofing-0.1.2.tar.gz
+cd twofing-0.1.2
+make
+sudo cp twofing /usr/bin/
+sudo cat /proc/bus/input/devices # Find the device name to use as matching string, find Vendor and Product IDs
+sudo bash -c 'cat << EOF > /etc/udev/rules.d/70-touchscreen-argonautM7.rules
+SUBSYSTEMS=="usb",ACTION=="add",KERNEL=="event*",ATTRS{idVendor}=="0000",ATTRS{idProduct}=="0009",SYMLINK+="twofingtouch",RUN+="/bin/chmod a+r /dev/twofingtouch"
+KERNEL=="event*",ATTRS{name}=="Argonaut. Touchscreen",SYMLINK+="twofingtouch",RUN+="/bin/chmod a+r /dev/twofingtouch"
+EOF'
+sudo apt install xserver-xorg-input-evdev xinput-calibrator
+sudo bash -c 'cat << EOF > /usr/share/X11/xorg.conf.d/90-touchinput.conf
+Section "InputClass"
+    Identifier "calibration"
+    Driver "evdev"
+    MatchProduct "Argonaut. Touchscreen"
+    MatchDevicePath "/dev/input/event*"
+    Option "Emulate3Buttons" "True"
+    Option "EmulateThirdButton" "1"
+    Option "EmulateThirdButtonTimeout" "750"
+    Option "EmulateThirdButtonMoveThreshold" "30"
+EndSection
+EOF'
+sudo bash -c 'cat << EOF > /etc/udev/rules.d/99-input-tagging.rules
+ACTION=="add", KERNEL=="event*", SUBSYSTEM=="input", TAG+="systemd", , ENV{SYSTEMD_ALIAS}+="/sys/subsystem/input/devices/$env{ID_SERIAL}"
+EOF'
+cat << EOF >> ~/.config/autostart/twofing-startup.desktop
+[Desktop Entry]
+Type=Application
+Name=Twofing
+Exec=twofing
+StartupNotify=false
+EOF
+reboot
