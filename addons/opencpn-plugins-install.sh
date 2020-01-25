272af2b76e7f2b0881d79e7177f6a851174310aa
mgrouch <mgrouch@users.noreply.github.com>
2020-01-25 07:19:45 -0500

    more documentation

---
+++ addons/opencpn-plugins-install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# install OpenCPN plugins
+
+sudo apt install                    \
+opencpn-plugin-calculator           \
+opencpn-plugin-celestial            \
+opencpn-plugin-chartscale           \
+opencpn-plugin-climatology-data     \
+opencpn-plugin-climatology          \
+opencpn-plugin-draw                 \
+opencpn-plugin-iacfleet             \
+opencpn-plugin-launcher             \
+opencpn-plugin-logbookkonni         \
+opencpn-plugin-nmeaconverter        \
+opencpn-plugin-objsearch            \
+opencpn-plugin-ocpndebugger         \
+opencpn-plugin-plots                \
+opencpn-plugin-polar                \
+opencpn-plugin-pypilot              \
+opencpn-plugin-radar                \
+opencpn-plugin-s63                  \
+opencpn-plugin-sar                  \
+opencpn-plugin-shipdriver           \
+opencpn-plugin-tactics              \
+opencpn-plugin-vfkaps               \
+opencpn-plugin-watchdog             \
+opencpn-plugin-weatherfax           \
+opencpn-plugin-weatherrouting
