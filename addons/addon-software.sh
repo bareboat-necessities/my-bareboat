48d41df8ad74e35826e893da5085c4ac29759118
mgrouch <mgrouch@users.noreply.github.com>
2020-01-28 20:17:42 -0500

    more documentation

--- addons/addon-software.sh
+++ addons/addon-software.sh
@@ -3,11 +3,18 @@
 # enable aux audio
 amixer cset numid=3 1
 
-sudo apt install nmap predict git gpsd xmlstarlet jq
+sudo apt install nmap predict git gpsd xmlstarlet
 
+sudo apt install jq
 sudo bash -c 'cat << EOF > /usr/bin/gps-loc
 #!/bin/bash
 curl -s http://localhost:3000/signalk/v1/api/vessels/self/navigation/position/ | jq -M -jr '\''.value.latitude," ",.value.longitude','" ",.timestamp'\''
 EOF'
 sudo chmod +x /usr/bin/gps-loc
 
+sudo apt install uhubctl
+sudo bash -c 'cat << EOF > /lib/systemd/system-shutdown/hub-off.sh
+#!/bin/bash
+/sbin/uhubctl -l 2 -p 4 -a 0
+EOF'
+sudo chmod +x /lib/systemd/system-shutdown/hub-off.sh
