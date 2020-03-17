dbdb09cdddffcc4b4bb67494fe27e555f9e98ea7
mgrouch <mgrouch@users.noreply.github.com>
2020-03-17 15:47:55 -0400

    started work on m5stack (improved mem management)

--- m5-boat-display/m5-boat-display.ino
+++ m5-boat-display/m5-boat-display.ino
@@ -15,28 +15,56 @@ String back_button = EXIT;
 
 TinyGPSPlus gps;
 
+boolean prevValidLOC = false;
+boolean prevValidSOG = false;
+boolean prevValidCOG = false;
+
+float prevLAT;
+float prevLON;
+float prevSOG;
+float prevCOG;
+
+char prevLATprefix[6];
+char prevLONprefix[6];
+char prevLATsuffix[10];
+char prevLONsuffix[10];
+char prevSOGstr[10];
+char prevCOGstr[5];
+
+char LATprefix[6];
+char LONprefix[6];
+char LATsuffix[10];
+char LONsuffix[10];
+char SOGstr[10];
+char COGstr[5];
+
+char prevTime[20];
+char gpsTime[20];
+
 void setup() {
   #include <themes/dark.h>
   #include <themes/default.h>
+  initNA(prevTime);
+  initNA(gpsTime);
   ezt::setDebug(INFO);
   ez.begin();
 }
 
 void loop() {
-  ezMenu mainmenu("M5 Boat Display");
+  ezMenu mainmenu(F("M5 Boat Display"));
   mainmenu.txtSmall();
-  mainmenu.addItem("Location", mainmenu_location);
-  mainmenu.addItem("NMEA Debug", mainmenu_nmea_debug);
-  mainmenu.addItem("System Info", mainmenu_sys);
-  mainmenu.addItem("Built-in WiFi & Settings", ez.settings.menu);
-  mainmenu.upOnFirst("last|up");
-  mainmenu.downOnLast("first|down");
+  mainmenu.addItem(F("Location"), mainmenu_location);
+  mainmenu.addItem(F("NMEA Debug"), mainmenu_nmea_debug);
+  mainmenu.addItem(F("System Info"), mainmenu_sys);
+  mainmenu.addItem(F("Built-in WiFi & Settings"), ez.settings.menu);
+  mainmenu.upOnFirst(F("last|up"));
+  mainmenu.downOnLast(F("first|down"));
   mainmenu.run();
 }
 
 void mainmenu_nmea_debug() {
   ez.screen.clear();
-  ez.header.show("NMEA Debug");
+  ez.header.show(F("NMEA Debug"));
   ez.buttons.show("#" + back_button + "#");
   ez.canvas.font(&FreeSans9pt7b);
   const int MAX_LINES = 5;
@@ -48,7 +76,7 @@ void mainmenu_nmea_debug() {
 
 void mainmenu_location() {
   ez.screen.clear();
-  ez.header.show("Location");
+  ez.header.show(F("Location"));
   ez.buttons.show("#" + back_button + "#");
   ez.canvas.font(&FreeSans9pt7b);
   boolean done = nmea_loop(false, -1, on_nmea_sentence);  
@@ -65,17 +93,17 @@ boolean nmea_loop_interrupted() {
 boolean nmea_loop(boolean debug, int maxLines, void (&onMessage)(String&)) {
   WiFiClient client;
   if (!client.connect(host, port)) {
-    ez.canvas.println("connection failed");
+    ez.canvas.println(F("connection failed"));
     return false;
   }
-  if (debug) ez.canvas.println("connection succeeded");
+  if (debug) ez.canvas.println(F("connection succeeded"));
 
   int lines = 0;
   while (true) {
     unsigned long timeout = millis();
     while (client.available() <= 0) {
       if (millis() - timeout > 5000) {
-        ez.canvas.println(">>> client timeout!");
+        ez.canvas.println(F(">> client timeout!"));
         client.stop();
         return false;
       }
@@ -111,55 +139,62 @@ void on_nmea_sentence_debug(String& line) {
   ez.canvas.println(line);
 }
 
-void printSpeed(char* label, boolean valid, float value, boolean prevValid, float prevValue, char* ending) {
+void printSpeed(char* label, boolean valid, float value, boolean prevValid, float prevValue, char* ending, char* speed, char* prevSpeed) {
   printLabel(label);
-  String prevSpeed = formatSpeed(prevValue, prevValid, ending);
-  String speed = formatSpeed(value, valid, ending);
-  if (prevSpeed != speed) {
+  formatSpeed(prevSpeed, prevValue, prevValid, ending);
+  formatSpeed(speed, value, valid, ending);
+  if (strcmp(prevSpeed, speed)) {
     erase(prevSpeed);
   }
   print(speed);
   ez.canvas.println();
 }
 
-String formatSpeed(float value, boolean valid, char* ending) {
-  return valid ? String(value, 1) + ' ' + ending : String('-');  
+void formatSpeed(char* valueStr, float value, boolean valid, char* ending) {
+  if (!valid) {
+    initNA(valueStr);
+  } else {
+    sprintf(valueStr, "%.1f %s", value, ending);
+  }  
 }
 
-void printAngle(char* label, boolean valid, float value, boolean prevValid, float prevValue) {
+void printAngle(char* label, boolean valid, float value, boolean prevValid, float prevValue, char* valueStr, char* prevValueStr) {
   printLabel(label);
-  String prevAngle = formatAngle(prevValue, prevValid);
-  String angle = formatAngle(value, valid);
-  if (prevAngle != angle) {
-    erase(prevAngle);
+  formatAngle(prevValueStr, prevValue, prevValid);
+  formatAngle(valueStr, value, valid);
+  if (strcmp(prevValueStr, valueStr)) {
+    erase(prevValueStr);
   }
-  print(angle);
+  print(valueStr);
   if (valid) {
     printDegree();
   }
   ez.canvas.println();
 }
 
-String formatAngle(float value, boolean valid) {
-  String str = String(value > 0 ? value : -value, 0);
-  str.trim();
-  return valid ? str : String('-');  
+void formatAngle(char* valueStr, float value, boolean valid) {
+  if (!valid) {
+    initNA(valueStr);
+  } else {
+    sprintf(valueStr, "%d", (int) value);
+  }  
 }
 
-void printCoord(char* label, boolean valid, float value, boolean prevValid, float prevValue, char a, char b) {
+void printCoord(char* label, boolean valid, float value, boolean prevValid, float prevValue, char a, char b, 
+                char* prefix, char* prevPrefix, char* suffix, char* prevSuffix) {
   printLabel(label);
-  String prefix = coordPrefix(value, valid, a, b);
-  String prevPrefix = coordPrefix(prevValue, prevValid, a, b);
-  String suffix = coordSuffix(value, valid);
-  String prevSuffix = coordSuffix(prevValue, prevValid);
-  if (prevPrefix != prefix) {
+  coordPrefix(prefix, value, valid, a, b);
+  coordPrefix(prevPrefix, prevValue, prevValid, a, b);
+  coordSuffix(suffix, value, valid);
+  coordSuffix(prevSuffix, prevValue, prevValid);
+  if (strcmp(prevPrefix, prefix)) {
     erase(prevPrefix);
   }
   print(prefix);
   if (valid) {
     printDegree();
   }
-  if (prevSuffix != suffix) {
+  if (strcmp(prevSuffix, suffix)) {
     erase(prevSuffix);
   }
   print(suffix);
@@ -180,39 +215,33 @@ void eraseDegree() {
   printDegreeWithColor(ez.theme->background);
 }
 
-String coordPrefix(float value, boolean valid, char a, char b) {
+void coordPrefix(char* valueStr, float value, boolean valid, char a, char b) {
   if (!valid) {
-    return String('-');
+    initNA(valueStr);
+    return;
   }
-  String result = String(a);
+  char p = a;
   if (value < 0) {
-    result = String(b);
+    p = b;
     value = (-value);
   }
-  result += ' ';
-  int deg = value;
-  String degrees = "";
-  if (deg < 100) degrees += '0';
-  if (deg < 10) degrees += '0';
-  degrees = degrees + deg;
-  return result + degrees;  
+  sprintf(valueStr, "%c %03d", p, (int)value);
 }
 
-String coordSuffix(float value, boolean valid) {
+void coordSuffix(char* valueStr, float value, boolean valid) {
   if (!valid) {
-    return String();
+    valueStr[0] = '\0';
+    return;
   }
   float deg = value;
   if (deg < 0) {
     deg = (-deg);
   }
   float minutesRemainder = (deg - ((int)deg)) * 60.0;
-  String minutes = "";
-  if (minutesRemainder < 10) minutes = minutes + "0";
-  return minutes + String(minutesRemainder, 5) + '\'';
+  sprintf(valueStr, "%08.5f", minutesRemainder);
 }
 
-void erase(String& value) {  
+void erase(char* value) {  
   uint16_t fg = ez.canvas.color();
   uint16_t bg = ez.theme->background;
   int x = ez.canvas.x();
@@ -225,34 +254,23 @@ void erase(String& value) {
   ez.canvas.y(y);
 }
 
-void print(String& value) {
+void print(char* value) {
   ez.canvas.print(value);
 }
 
 void printLabel(char* label) {
-  ez.canvas.print(label);  
-  ez.canvas.print(": ");
+  print(label);  
+  print(": ");
 }
 
-void printTime(char* label, boolean valid, String& value, boolean prevValid, String& prevValue) {
+void printTime(char* label, char* value, char* prevValue) {
   printLabel(label);
-  if (prevValue != value) {
+  if (strcmp(prevValue, value)) {
     erase(prevValue);
   }
   print(value);
   ez.canvas.println();
 }
-
-boolean prevValidLOC = false;
-boolean prevValidSOG = false;
-boolean prevValidCOG = false;
-boolean prevValidTime = false;
-
-float prevLAT;
-float prevLON;
-float prevSOG;
-float prevCOG;
-String prevTime = String('-');
 
 void displayInfo() {
   ez.canvas.font(&FreeMonoBold12pt7b);
@@ -261,34 +279,38 @@ void displayInfo() {
   ez.canvas.x(ez.canvas.lmargin());
 
   boolean locValid = gps.location.isValid();
-  printCoord("LAT", locValid, gps.location.lat(), prevValidLOC, prevLAT, 'N', 'S'); 
+  printCoord("LAT", locValid, gps.location.lat(), prevValidLOC, prevLAT, 'N', 'S', 
+             LATprefix, prevLATprefix, LATsuffix, prevLATsuffix); 
   prevLAT = gps.location.lat();
   
-  printCoord("LON", locValid, gps.location.lng(), prevValidLOC, prevLON, 'E', 'W'); 
+  printCoord("LON", locValid, gps.location.lng(), prevValidLOC, prevLON, 'E', 'W',
+             LONprefix, prevLONprefix, LONsuffix, prevLONsuffix); 
   prevLON = gps.location.lng();
   prevValidLOC = locValid;
   
-  printSpeed("SOG", gps.speed.isValid(), gps.speed.knots(), prevValidSOG, prevSOG, "kt"); 
+  printSpeed("SOG", gps.speed.isValid(), gps.speed.knots(), prevValidSOG, prevSOG, "kt", SOGstr, prevSOGstr); 
   prevSOG = gps.speed.knots(); 
   prevValidSOG = gps.speed.isValid();
   
-  printAngle("COG", gps.course.isValid(), gps.course.deg(), prevValidCOG, prevCOG); 
+  printAngle("COG", gps.course.isValid(), gps.course.deg(), prevValidCOG, prevCOG, COGstr, prevCOGstr); 
   prevCOG = gps.course.deg(); 
   prevValidCOG = gps.course.isValid();
 
-  String gpsTime = toTimestamp(gps.time.isValid(), gps.time, gps.date);
-  printTime("UTC", gps.time.isValid(), gpsTime, prevValidTime, prevTime); 
-  prevTime = gpsTime; 
-  prevValidTime = gps.time.isValid();
+  toTimestamp(gpsTime, gps.time.isValid(), gps.time, gps.date);
+  printTime("UTC", gpsTime, prevTime); 
+  strcpy(prevTime, gpsTime); 
 }
 
-String toTimestamp(boolean valid, TinyGPSTime& time, TinyGPSDate& date) {
+void toTimestamp(char* buf, boolean valid, TinyGPSTime& time, TinyGPSDate& date) {
   if (!valid) {
-    return String('-');
+    initNA(buf);
   }
-  char buf[20];
   sprintf(buf, "%02d/%02d %02d:%02d:%02d", date.day(), date.month(), time.hour(), time.minute(), time.second());
-  return String(buf);  
+}
+
+void initNA(char* buf) {
+  buf[0] = '-';
+  buf[1] = '\0';
 }
 
 void mainmenu_sys() {
@@ -296,16 +318,16 @@ void mainmenu_sys() {
   images.imgBackground(TFT_BLACK);
   images.imgFromTop(40);
   images.imgCaptionColor(TFT_WHITE);
-  images.addItem(wifi_jpg, "WiFi Settings", ez.wifi.menu);
-  images.addItem(sysinfo_jpg, "System Status", sysInfo);
-  images.addItem(about_jpg, "About M5 Boat Display", aboutM5boat);
-  images.addItem(sleep_jpg, "Power Off", powerOff);
-  images.addItem(return_jpg, "Back");
+  images.addItem(wifi_jpg, F("WiFi Settings"), ez.wifi.menu);
+  images.addItem(sysinfo_jpg, F("System Status"), sysInfo);
+  images.addItem(about_jpg, F("About M5 Boat Display"), aboutM5boat);
+  images.addItem(sleep_jpg, F("Power Off"), powerOff);
+  images.addItem(return_jpg, F("Back"));
   images.run();
 }
 
 void powerOff() { m5.powerOFF(); }
 
 void aboutM5boat() {
-  ez.msgBox("About M5 Boat Display", "By Bareboat-Necessities | | bareboat-necessities.github.io");
+  ez.msgBox(F("About M5 Boat Display"), F("By Bareboat-Necessities | | bareboat-necessities.github.io"));
 }
