997684d210d02a43dfe4f00a7d9b79c47a57feae
mgrouch <mgrushinskiy@gmail.com>
2020-03-24 22:15:34 -0400

    ais

--- modules/ais-processor/src/main/java/com/bareboat_necessities/ais/Boot.java
+++ modules/ais-processor/src/main/java/com/bareboat_necessities/ais/Boot.java
@@ -1,15 +1,21 @@
 package com.bareboat_necessities.ais;
 
 import dk.tbsalling.aismessages.AISInputStreamReader;
+import dk.tbsalling.aismessages.ais.messages.AISMessage;
 
 import java.io.IOException;
 
 public class Boot {
+
     public static void main(String[] args) throws IOException {
         var classLoader = Thread.currentThread().getContextClassLoader();
         try (var inputStream = classLoader.getResourceAsStream("nmea-ais-sample.txt")) {
-            var streamReader = new AISInputStreamReader(inputStream, System.out::println);
+            var streamReader = new AISInputStreamReader(inputStream, Boot::process);
             streamReader.run();
         }
+    }
+
+    static void process(AISMessage msg) {
+        System.out.println(msg);
     }
 }
