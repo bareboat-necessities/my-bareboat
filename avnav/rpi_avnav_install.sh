c40b2609f849fbbe117a7edbb381897641470077
mgrouch <mgrouch@users.noreply.github.com>
2020-03-26 21:15:36 -0400

    more doc

---
+++ avnav/rpi_avnav_install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+sudo wget -q -O - https://open-mind.space/repo/open-mind.space.gpg.key |  sudo apt-key add -
+
+sudo bash -c 'cat << EOF > /etc/apt/sources.list.d/extra.list
+deb https://open-mind.space/repo/ buster-stable avnav
+EOF'
+
+sudo apt update
+sudo apt install avnav
+
+# sudo nano /var/lib/avnav/avnav_server.xml
+#
+# <AVNSocketReader host="localhost" port="10110"/>
+#
+#
+# <AVNHttpServer navurl="/viewer/avnav_navi.php" httpPort="8099" upzoom="0"
+#
+# sudo systemctl start avnav
---
+++ qtvlm/rpi_qtvlm_install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+wget https://www.meltemus.com/index.php/en/download/send/9-raspberrypi/197-qtvlm-5-9-7-6
+mv 197-qtvlm-5-9-7-6 qtVlm-5.9.7-p2-rpi.tar.gz
+
+gzip -cd < qtVlm-5.9.7-p2-rpi.tar.gz | tar xvf -
+
+# cd qtVlm
+# ./qtVlm_touchscreen -platform xcb
