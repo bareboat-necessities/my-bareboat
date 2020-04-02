f5bd2f99d43160ff93d798044a136da3ab763ff8
mgrouch <mgrushinskiy@gmail.com>
2020-04-01 20:17:12 -0400

    qtvlm

--- qtvlm/rpi_qtvlm_install.sh
+++ qtvlm/rpi_qtvlm_install.sh
@@ -2,10 +2,9 @@
 
 # See: https://www.meltemus.com
 
-wget https://www.meltemus.com/index.php/en/download/send/9-raspberrypi/197-qtvlm-5-9-7-6
-mv 197-qtvlm-5-9-7-6 qtVlm-5.9.7-p2-rpi.tar.gz
-
+curl https://www.meltemus.com/index.php/en/download/send/9-raspberrypi/197-qtvlm-5-9-7-6 > qtVlm-5.9.7-p2-rpi.tar.gz
 gzip -cd < qtVlm-5.9.7-p2-rpi.tar.gz | tar xvf -
+curl https://raw.githubusercontent.com/bareboat-necessities/my-bareboat/master/qtvlm-conf/qtVlm.ini > /home/pi/.qtVlm/qtVlm.ini
 
 # cd qtVlm
-# ./qtVlm_touchscreen -platform xcb
+# ./qtVlm -platform xcb
