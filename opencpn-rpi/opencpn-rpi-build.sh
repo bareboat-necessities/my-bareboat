e17b85049a6dd64f6837d915c3a1d57bef5a503c
mgrouch <mgrushinskiy@gmail.com>
2020-10-06 00:26:06 -0400

    more doc

--- opencpn-rpi/opencpn-rpi-build.sh
+++ opencpn-rpi/opencpn-rpi-build.sh
@@ -3,6 +3,7 @@
 sudo apt-get -y install build-essential cmake gettext git-core gpsd gpsd-clients libgps-dev \
  libglu1-mesa-dev libgtk-3-dev libbz2-dev libtinyxml-dev libportaudio2 portaudio19-dev libcurl4-openssl-dev \
  libexpat1-dev libcairo2-dev libarchive-dev liblzma-dev libexif-dev libelf-dev libsqlite3-dev \
+# See ../wxWidgets-rpi/
 # wx-common libwxgtk3.0-dev wx3.0-headers \
  bc bison flex libssl-dev python3 ddd htop
 
