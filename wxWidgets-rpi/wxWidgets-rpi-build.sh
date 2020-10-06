2a42fdb47dcfff21f1c565533d34216dd528466c
mgrouch <mgrushinskiy@gmail.com>
2020-10-06 00:33:19 -0400

    more doc

--- wxWidgets-rpi/wxWidgets-rpi-build.sh
+++ wxWidgets-rpi/wxWidgets-rpi-build.sh
@@ -3,10 +3,10 @@
 sudo apt-get -y install build-essential cmake gettext git-core libgtk-3-dev libgtk-3-0
 
 git clone -b v3.1.4 --recurse-submodules https://github.com/wxWidgets/wxWidgets.git
-cd wxWidgets || exit
+cd wxWidgets || exit 255
 
 mkdir build-gtk
-cd build-gtk || exit
+cd build-gtk || exit 255
 
 ../configure --with-gtk=3 --with-libpng=builtin --with-libjpeg=builtin --with-libtiff=builtin --with-regex=builtin \
  --with-zlib=builtin --with-expat=builtin --enable-unicode --enable-aui --with-opengl \
