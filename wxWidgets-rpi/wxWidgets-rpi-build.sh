66df288e59ab3ebc8be7d5a96210dfef0c8b9ab5
mgrouch <mgrushinskiy@gmail.com>
2020-10-07 12:39:21 -0400

    more doc

--- wxWidgets-rpi/wxWidgets-rpi-build.sh
+++ wxWidgets-rpi/wxWidgets-rpi-build.sh
@@ -2,7 +2,7 @@
 
 sudo apt-get -y install build-essential cmake gettext git-core libgtk-3-dev libgtk-3-0 libgl1-mesa-dev libglu1-mesa-dev \
  zlib1g-dev libjpeg-dev libpng-dev libtiff5-dev libsm-dev autotools-dev autoconf libexpat1-dev libxt-dev \
- libgstreamer1.0-dev, libgstreamer-plugins-base1.0-dev libwebkit2gtk-4.0-dev libnotify-dev
+ libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev libwebkit2gtk-4.0-dev libnotify-dev
 
 git clone -b v3.1.4 --recurse-submodules https://github.com/wxWidgets/wxWidgets.git
 cd wxWidgets || exit 255
