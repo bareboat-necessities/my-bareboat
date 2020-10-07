b4f1f2e5ad98764a6c7a36b40c338b7bb5c0ee65
mgrouch <mgrushinskiy@gmail.com>
2020-10-07 12:35:33 -0400

    more doc

--- wxWidgets-rpi/wxWidgets-rpi-build.sh
+++ wxWidgets-rpi/wxWidgets-rpi-build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-sudo apt-get -y install build-essential cmake gettext git-core libgtk-3-dev libgtk-3-0 libglu1-mesa-dev
+sudo apt-get -y install build-essential cmake gettext git-core libgtk-3-dev libgtk-3-0 libgl1-mesa-dev libglu1-mesa-dev \
+ zlib1g-dev libjpeg-dev libpng-dev libtiff5-dev libsm-dev autotools-dev autoconf libexpat1-dev libxt-dev \
+ libgstreamer1.0-dev, libgstreamer-plugins-base1.0-dev libwebkit2gtk-4.0-dev libnotify-dev
 
 git clone -b v3.1.4 --recurse-submodules https://github.com/wxWidgets/wxWidgets.git
 cd wxWidgets || exit 255
@@ -8,9 +10,10 @@ cd wxWidgets || exit 255
 mkdir build-gtk
 cd build-gtk || exit 255
 
-../configure --with-gtk=3 --with-libpng=builtin --with-libjpeg=builtin --with-libtiff=builtin --with-regex=builtin \
+../configure --with-gtk=3 --with-libpng=sys --with-libjpeg=sys --with-libtiff=sys --with-regex=builtin \
+ --enable-display --enable-geometry --enable-graphics_ctx --enable-mediactrl --enable-sound \
  --with-zlib=builtin --with-expat=builtin --enable-unicode --enable-aui --with-opengl \
- LDFLAGS="-static-libgcc -static-libstdc++"
+ --enable-debug --enable-exceptions --enable-webview LDFLAGS="-static-libgcc -static-libstdc++"
 make -sj5
 sudo make install
 sudo ldconfig
