00bbe870186b9e9f2cc35a4a04b4eb4cdac63b10
mgrouch <mgrushinskiy@gmail.com>
2020-10-09 16:43:03 -0400

    more doc

--- wxWidgets-rpi/wxWidgets-rpi-build.sh
+++ wxWidgets-rpi/wxWidgets-rpi-build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-sudo apt-get -y install build-essential cmake gettext git-core libgtk-3-dev libgtk-3-0 libgl1-mesa-dev libglu1-mesa-dev \
+sudo apt-get -y install build-essential cmake gettext git-core libgtk-3-dev libgl1-mesa-dev libglu1-mesa-dev \
+ libgtk2.0-dev \
  zlib1g-dev libjpeg-dev libpng-dev libtiff5-dev libsm-dev autotools-dev autoconf libexpat1-dev libxt-dev \
  libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev libwebkit2gtk-4.0-dev libnotify-dev
 
@@ -9,11 +10,11 @@ cd wxWidgets || exit 255
 
 #git checkout 301e5ec9fe3719fac8a2786e8675b19e2242183e
 
-mkdir build-gtk
-cd build-gtk || exit 255
+mkdir build-gtk2
+cd build-gtk2 || exit 255
 
 ../configure \
- --with-gtk=3 \
+ --with-gtk=2 \
  --with-libpng=sys \
  --with-libjpeg=sys \
  --with-libtiff=sys \
