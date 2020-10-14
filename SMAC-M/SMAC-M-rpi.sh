a0b6239cfb5c6723a513866beac399b10c1350be
mgrouch <mgrushinskiy@gmail.com>
2020-10-14 01:29:14 -0400

    fixes for docs

--- SMAC-M/SMAC-M-rpi.sh
+++ SMAC-M/SMAC-M-rpi.sh
@@ -2,20 +2,19 @@
 
 # See https://github.com/LarsSchy/SMAC-M
 
-sudo apt-get y install python3 python3-pip python3-venv gdal-bin \
+sudo apt-get -y install python3 python3-pip python3-venv gdal-bin \
  python3-gdal build-essential libgdal-dev imagemagick xmlstarlet
 echo 'PATH="$HOME/.local/bin/:$PATH"' >>~/.bashrc
 pip3 install --user pipenv
-bash
 
-pipenv install
 git clone https://github.com/LarsSchy/SMAC-M
 cd ./SMAC-M || exit 255
-
 pipenv install
 pipenv run pip install "GDAL<=$(gdal-config --version)"
 
-pipenv shell < EOF
+#pipenv shell
+
+source "$(pipenv --venv)/bin/activate"
 ogrinfo --version
 wget https://raw.githubusercontent.com/OpenCPN/OpenCPN/master/data/s57data/s57objectclasses.csv
 wget https://raw.githubusercontent.com/OpenCPN/OpenCPN/master/data/s57data/s57attributes.csv
@@ -24,5 +23,13 @@ sudo cp s57attributes.csv /usr/share/gdal/s57attributes_iw.csv
 export S57_PROFILE=iw
 export OGR_S57_OPTIONS=SPLIT_MULTIPOINT=ON,ADD_SOUNDG_DEPTH=ON
 ogrinfo --config S57_PROFILE iw -ro ./datatest/US5OH10M/US5OH10M.000 | grep "SLOTOP"
-EOF
+
+
+
+
+
+
+
+
+
 
