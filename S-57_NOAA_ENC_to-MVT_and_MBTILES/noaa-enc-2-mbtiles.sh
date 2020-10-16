265a93be2585dc73646bff5ae5ec23b56c17e282
mgrouch <mgrushinskiy@gmail.com>
2020-10-16 00:21:40 -0400

    fixes for docs

--- S-57_NOAA_ENC_to-MVT_and_MBTILES/noaa-enc-2-mbtiles.sh
+++ S-57_NOAA_ENC_to-MVT_and_MBTILES/noaa-enc-2-mbtiles.sh
@@ -24,7 +24,8 @@ wget https://raw.githubusercontent.com/OpenCPN/OpenCPN/master/data/s57data/s57at
 rm -rf *-mvt *.temp.db || true
 find . -name "US*.000" -type f |
 while read -r in
-  do  ogr2ogr -append -skipfailures -f MVT -dsco FORMAT=DIRECTORY -dsco MAXZOOM=${MAXZOOM} \
+do
+    ogr2ogr -append -skipfailures -f MVT -dsco FORMAT=DIRECTORY -dsco MAXZOOM=${MAXZOOM} \
     -explodecollections \
     --config S57_PROFILE iw --config OGR_S57_OPTIONS \
     -mapFieldType StringList\|All=JSonStringList,IntegerList\|All=JSonIntegerList \
