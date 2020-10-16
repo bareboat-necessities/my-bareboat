f5d047fd669ca067d89a826ab97ec2d9b1177245
mgrouch <mgrushinskiy@gmail.com>
2020-10-15 23:47:30 -0400

    fixes for docs

--- S-57_NOAA_ENC_to-MVT_and_MBTILES/noaa-enc-2-mbtiles.sh
+++ S-57_NOAA_ENC_to-MVT_and_MBTILES/noaa-enc-2-mbtiles.sh
@@ -22,69 +22,7 @@ wget https://raw.githubusercontent.com/OpenCPN/OpenCPN/master/data/s57data/s57at
 rm -rf *-mvt *.temp.db || true
 find . -name "US*.000" -type f | while read -r in; do  ogr2ogr -append -skipfailures -f MVT -dsco FORMAT=DIRECTORY -dsco MAXZOOM=5 `basename $in .000`-mvt $in; done
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# To MapBox MVT directory
-find . -name "US*.000" -exec ogr2ogr -append -skipfailures -f MVT -dsco FORMAT=DIRECTORY -dsco MAXZOOM=5 {}  \;
-
-# To MapBox .mbtiles
-find . -name "US*.000" -exec ogr2ogr -append -skipfailures -f MVT -dsco FORMAT=DIRECTORY -dsco MAXZOOM=5 `echo $(basename {})`.mbtiles {}  \;
-
-ogr2ogr -append -skipfailures -f MVT -dsco FORMAT=DIRECTORY -dsco MAXZOOM=5
-
-
- -f MVT -dsco FORMAT=MBTILES -dsco MAXZOOM=5 -splitlistfields ./mvt4.mbtiles ./datatest/US5OH10M/US5OH10M.000
-
-
-
-
-find . -name "*.000" -type f |
-while read -r in;
-do
-  ogr2ogr -append -skipfailures -f MVT -dsco FORMAT=DIRECTORY -dsco MAXZOOM=5 `basename $in .000` $in
-done
-
-
-find . -name "*.000" -type f |
-while read -r f
-do
-    echo "f"
-done
-
-
-
-ogr2ogr -skipfailures -f MVT -dsco FORMAT=DIRECTORY -dsco MAXZOOM=5 ./mvt66 ./datatest/US5OH10M/US5OH10M.000
-
-
-ogr2ogr -append -skipfailures -f MVT -dsco FORMAT=DIRECTORY -dsco MAXZOOM=5 ./mvt12 ./datatest/US5OH10M/US5OH10M.000
-
-
-
-
-
 # Displaying projection CRS EPSG:3857
 # WSG 84 Pseudo-Mercator
-
 
 
