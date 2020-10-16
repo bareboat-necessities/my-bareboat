966070da58df2e3299e2881965f06022ef88a2a0
mgrouch <mgrushinskiy@gmail.com>
2020-10-16 00:19:35 -0400

    fixes for docs

--- S-57_NOAA_ENC_to-MVT_and_MBTILES/noaa-enc-2-mbtiles.sh
+++ S-57_NOAA_ENC_to-MVT_and_MBTILES/noaa-enc-2-mbtiles.sh
@@ -22,12 +22,13 @@ wget https://raw.githubusercontent.com/OpenCPN/OpenCPN/master/data/s57data/s57ob
 wget https://raw.githubusercontent.com/OpenCPN/OpenCPN/master/data/s57data/s57attributes.csv
 
 rm -rf *-mvt *.temp.db || true
-find . -name "US*.000" -type f | while read -r in
+find . -name "US*.000" -type f |
+while read -r in
   do  ogr2ogr -append -skipfailures -f MVT -dsco FORMAT=DIRECTORY -dsco MAXZOOM=${MAXZOOM} \
     -explodecollections \
     --config S57_PROFILE iw --config OGR_S57_OPTIONS \
     -mapFieldType StringList\|All=JSonStringList,IntegerList\|All=JSonIntegerList \
-     `basename $in .000`-mvt $in
+     `basename $in .000`-mvt $in ;
 done
 
 # Displaying projection CRS EPSG:3857
