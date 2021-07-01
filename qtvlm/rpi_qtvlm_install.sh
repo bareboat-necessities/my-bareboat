ff8465b23c0fba9d1e47725d98ccc50cb3ac7345
mgrouch <mgrushinskiy@gmail.com>
2021-07-01 13:17:20 -0400

    upd

--- qtvlm/rpi_qtvlm_install.sh
+++ qtvlm/rpi_qtvlm_install.sh
@@ -1,10 +1,34 @@
-#!/bin/bash
+#!/bin/bash -e
+
+myArch=$(dpkg --print-architecture)
+
+if [ "armhf" != "$myArch" ] ; then
+    echo "armhf version of the distribution is required. Exiting..."
+    exit 1
+fi
 
 # See: https://www.meltemus.com
 
-curl https://www.meltemus.com/index.php/en/download/send/9-raspberrypi/197-qtvlm-5-9-7-6 > qtVlm-5.9.7-p2-rpi.tar.gz
-gzip -cd < qtVlm-5.9.7-p2-rpi.tar.gz | tar xvf -
-curl https://raw.githubusercontent.com/bareboat-necessities/my-bareboat/master/qtvlm-conf/qtVlm.ini > /home/pi/.qtVlm/qtVlm.ini
+sudo apt-get -y install libsystemd0:armhf
+
+cd /home/user
+wget -q -O - "https://www.meltemus.com/index.php/en/download?task=download.send&id=325&catid=9&m=0" > qtVlm-5.10.3-rpi.tar.gz
+gzip -cd < qtVlm-5.10.3-rpi.tar.gz | tar xvf -
+mkdir /home/user/.qtVlm
+wget -q -O - https://raw.githubusercontent.com/bareboat-necessities/my-bareboat/master/qtvlm-conf/qtVlm.ini > /home/user/.qtVlm/qtVlm.ini
+
+sudo bash -c 'cat << EOF > /usr/local/share/applications/qtvlm.desktop
+[Desktop Entry]
+Type=Application
+Name=QtVlm
+GenericName=QtVlm
+Comment=QtVlm ChartPlotter
+Exec=sh -c "cd /home/user/qtVlm; ./qtVlm -platform xcb"
+Terminal=false
+Icon=/home/user/qtVlm/icon/qtVlm_48x48.png
+Categories=Navigation;ChartPlotter
+Keywords=Navigation;ChartPlotter
+EOF'
 
 # cd qtVlm
 # ./qtVlm -platform xcb
