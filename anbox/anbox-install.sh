6c86585ee0c748667f7aff7dcca623bd2cc2396a
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2021-09-09 16:04:51 +0100

    Update anbox-install.sh

--- anbox/anbox-install.sh
+++ anbox/anbox-install.sh
@@ -8,6 +8,7 @@ sudo apt install build-essential cmake cmake-data debhelper dbus google-mock \
     libboost-thread-dev libcap-dev libexpat1-dev libsystemd-dev libegl1-mesa-dev \
     libgles2-mesa-dev libglm-dev libgtest-dev liblxc1 \
     libproperties-cpp-dev libprotobuf-dev libsdl2-dev libsdl2-image-dev lxc-dev \
+    libdw-dev libdwarf-dev binutils-dev \
     pkg-config protobuf-compiler python3-minimal
     
 git clone https://github.com/anbox/anbox.git
