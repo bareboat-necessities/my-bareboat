1f23efe9f2d8d796d590bddd00c7f41a19065586
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2021-09-09 22:04:09 +0100

    Update anbox-install.sh

--- anbox/anbox-install.sh
+++ anbox/anbox-install.sh
@@ -15,9 +15,11 @@ sudo apt install build-essential cmake cmake-data debhelper dbus google-mock \
 git clone https://github.com/anbox/anbox.git
 cd anbox
 #git submodule init
+git submodule update --init
+
 #git reset --hard 31d584e13d24b1889566ae77e7cabc88e6414ebf
 git reset --hard 3ed2e6d5c360d57b6aa61386e279adf3ff155ded
-git submodule update --init
+
 wget -O anbox-patch.zip "https://www.raspberrypi.org/forums/download/file.php?id=47177&sid=6efa5aaef4401f0bc981550e9a3dd499"
 unzip anbox-patch.zip
 git apply anbox-patch.diff
