b75326ee7458e3fd8d0f2c9f56fb3717f4384a1c
mgrouch <mgrushinskiy@gmail.com>
2021-11-07 14:48:59 -0500

    upd

---
+++ homeassistant/homeassistant-install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -e
+
+sudo apt-get install -y python3 python3-dev python3-venv python3-pip libffi-dev libssl-dev libjpeg-dev \
+   zlib1g-dev autoconf build-essential libopenjp2-7 libtiff5 libturbojpeg tzdata
+
+sudo useradd -rm homeassistant -G dialout,gpio,i2c
+
+sudo mkdir -p /srv/homeassistant
+sudo chown homeassistant:homeassistant /srv/homeassistant
+
+sudo -u homeassistant -H -s
+cd /srv/homeassistant
+python3.8 -m venv .
+source bin/activate
+
+python3 -m pip install wheel
+
+pip3 install homeassistant
+
+hass
+
