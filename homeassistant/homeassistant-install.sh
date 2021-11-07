4b17fd1f59db2fefb4b1c99ade23305e137e2950
mgrouch <mgrushinskiy@gmail.com>
2021-11-07 14:54:20 -0500

    fix

--- homeassistant/homeassistant-install.sh
+++ homeassistant/homeassistant-install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 sudo apt-get install -y python3 python3-dev python3-venv python3-pip libffi-dev libssl-dev libjpeg-dev \
-   zlib1g-dev autoconf build-essential libopenjp2-7 libtiff5 libturbojpeg tzdata
+   zlib1g-dev autoconf build-essential libopenjp2-7 libtiff5 libturbojpeg0 tzdata
 
 sudo useradd -rm homeassistant -G dialout,gpio,i2c
 
