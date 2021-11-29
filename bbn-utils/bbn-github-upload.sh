a6fcd74b25dcd4f946a3c3ade7d5fcc37e648318
mgrouch <mgrushinskiy@gmail.com>
2021-11-28 19:43:35 -0500

    upd

--- bbn-utils/bbn-github-upload.sh
+++ bbn-utils/bbn-github-upload.sh
@@ -1,9 +1,18 @@
 #!/bin/bash -e
 
 FILE=$1
+GITHUB_TOKEN=
 
-curl \
-    -H "Authorization: token $GITHUB_TOKEN" \
+curl -X POST \
+    -H "Content-Length: $(stat --format=%s $FILE)" \
     -H "Content-Type: $(file -b --mime-type $FILE)" \
-    --data-binary @$FILE \
-    "https://uploads.github.com/repos/bareboat-necessities/lysmarine_gen/releases/50516401/assets?name=$(basename $FILE)"
+    -T "$FILE" \
+    -H "Authorization: token $GITHUB_TOKEN" \
+    -H "Accept: application/vnd.github.v3+json" \
+    "https://uploads.github.com/repos/bareboat-necessities/lysmarine_gen/releases/54202060/assets?name=$(basename $FILE)" | cat
+
+curl -X GET \
+    -H "Authorization: token $GITHUB_TOKEN" \
+    -H "Accept: application/vnd.github.v3+json" \
+    "https://api.github.com/repos/bareboat-necessities/lysmarine_gen/releases/54202060/assets"
+
