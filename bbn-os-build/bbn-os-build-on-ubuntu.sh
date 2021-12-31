7193a106b41d59e655ccdb12c20b83822c87f138
mgrouch <mgrushinskiy@gmail.com>
2021-12-30 19:49:30 -0500

    upd

--- bbn-os-build/bbn-os-build-on-ubuntu.sh
+++ bbn-os-build/bbn-os-build-on-ubuntu.sh
@@ -19,7 +19,7 @@ mkdir $WORK_DIR && cd $WORK_DIR
 #git clone --branch buster https://github.com/bareboat-necessities/lysmarine_gen .
 git clone --branch incremental https://github.com/bareboat-necessities/lysmarine_gen .
 
-mv install-scripts cross-build-release/
+mv *install-scripts* cross-build-release/
 chmod a+x .circleci/*.sh
 
 .circleci/build-ci.sh 2>&1 | tee build.log
