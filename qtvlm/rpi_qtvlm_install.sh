9fa3502236a01d58b7bef9f2a957232664feea1b
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2022-01-12 01:23:42 +0000

    Update rpi_qtvlm_install.sh

--- qtvlm/rpi_qtvlm_install.sh
+++ qtvlm/rpi_qtvlm_install.sh
@@ -2,9 +2,9 @@
 
 myArch=$(dpkg --print-architecture)
 
+FID=398
 if [ "armhf" != "$myArch" ] ; then
-    echo "armhf version of the distribution is required. Exiting..."
-    exit 1
+    FID=399
 fi
 
 # See: https://www.meltemus.com
@@ -13,8 +13,8 @@ sudo apt-get -y install libsystemd0:armhf
 
 cd /home/user
 echo "Downloading..."
-wget -q -O - "https://www.meltemus.com/index.php/en/download?task=download.send&id=378&catid=9&m=0" > qtVlm-5.10.9-p1-rpi.tar.gz
-gzip -cd < qtVlm-5.10.9-p1-rpi.tar.gz | tar xvf -
+wget -q -O - "https://www.meltemus.com/index.php/en/download?task=download.send&id=${FID}&catid=9&m=0" > qtVlm-5.10.9-p4-rpi.tar.gz
+gzip -cd < qtVlm-5.10.9-p4-rpi.tar.gz | tar xvf -
 mkdir /home/user/.qtVlm
 wget -q -O - https://raw.githubusercontent.com/bareboat-necessities/my-bareboat/master/qtvlm-conf/qtVlm.ini > /home/user/.qtVlm/qtVlm.ini
 
