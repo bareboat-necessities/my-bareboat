0fb8367ee2f95e4d6ddfe596ee147fa8ed15e153
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2022-07-28 20:29:53 +0100

    Update kiwix-install-buster.sh

--- wikipedia/kiwix-install-buster.sh
+++ wikipedia/kiwix-install-buster.sh
@@ -3,6 +3,7 @@
 wget https://download.kiwix.org/release/kiwix-tools/kiwix-tools_linux-armhf.tar.gz
 
 # See https://archive.org/details/@wikipediaarchives
+# and https://library.kiwix.org/
 
 mkdir ~/Wikipedia && cd ~/Wikipedia
 wget https://archive.org/download/wikipedia_en_all_novid_2018-10.zim_201904/wikivoyage_en_all_novid_2019-04.zim
