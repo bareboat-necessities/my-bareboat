16b13fa76dd05031e4b68eee605da70fcdafbebe
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2022-08-12 13:02:51 +0000

    Update box86-arm64-bullseye.sh

--- box86/box86-arm64-bullseye.sh
+++ box86/box86-arm64-bullseye.sh
@@ -1,93 +1,16 @@
 #!/usr/bin/bash -e
 
-# See: https://forum.armbian.com/topic/16584-install-box86-on-arm64/
+# See: https://forum.armbian.com/topic/19526-how-to-install-box86-box64-wine32-wine64-winetricks-on-arm64/
 
-sudo apt-get -y install schroot debootstrap
-
-sudo mkdir -p /srv/chroot
-sudo mkdir -p /srv/chroot/debian-armhf
-
-sudo debootstrap --arch armhf --foreign bullseye /srv/chroot/debian-armhf 
-
-sudo chroot "/srv/chroot/debian-armhf" /debootstrap/debootstrap --second-stage
-
-sudo bash -c 'cat << EOF > /etc/schroot/chroot.d/debian-armhf-on-arm64
-[debian-armhf-on-arm64]
-description=Debian Armhf chroot
-aliases=arm32
-type=directory
-directory=/srv/chroot/debian-armhf
-profile=desktop
-personality=linux
-preserve-environment=true
-root-users=root
-users=user
-EOF'
-
-sudo bash -c 'cat << EOF > /etc/schroot/desktop/nssdatabases
-shadow
-gshadow
-services
-protocols
-EOF'
-
-sudo bash -c 'cat << EOF > /srv/chroot/debian-armhf/var/lib/dpkg/statoverride
-root root 2755 /usr/bin/crontab
-EOF'
+sudo dpkg --add-architecture armhf
+sudo apt-get update
+sudo apt-get install git cmake cabextract gcc-arm-linux-gnueabihf libc6-dev-armhf-cross
 
 {
-cat << EOF >> ~/.bashrc
-export LANGUAGE="C"
-export LC_ALL="C"
-export DISPLAY=:0
-EOF
-} | sudo schroot -c arm32 -u root
-
-
-sudo schroot -c arm32 -u root -- bash -c "apt-get -y install git wget cmake build-essential python3 gcc-arm-linux-gnueabihf zenity"
-
-sudo schroot -c arm32 -u root -- bash -c "git clone https://github.com/ptitSeb/box86 && cd box86 && mkdir build; cd build; cmake .. -DARM_DYNAREC=ON -DCMAKE_BUILD_TYPE=RelWithDebInfo; make -j 4"
-
-sudo schroot -c arm32 -u root -- bash -c "wget https://twisteros.com/wine.tgz && tar zxvf wine.tgz"
-
-sudo schroot -c arm32 -u root -- bash -c "wget  http://siriuscyber.net/sailmail/amsm35054b.exe"
-
-exit 0
-
-sudo schroot -c arm32 -u root -- bash -c "~/box86/build/box86 ~/wine/bin/wine amsm35054b.exe"
-
-
-sudo schroot -c arm32 -u root -- bash -c "ls"
-
-
-
-{
- apt-get install sudo
- sudo apt-get update
- sudo apt-get upgrade
-} | sudo schroot -c arm32
-
-{
-cat << EOF >> ~/.bashrc
-export LANGUAGE="C"
-export LC_ALL="C"
-export DISPLAY=:0
-EOF
-} | sudo schroot -c arm32 -u root
-
-
-{
-adduser user --quiet --disabled-password 
-echo user:changeme | chpasswd
-} | sudo schroot -c arm32
-
-{
-sudo -u user bash -c 'cat << EOF >> ~/.bashrc
-export LANGUAGE="C"
-export LC_ALL="C"
-export DISPLAY=:0
-EOF'  
-} | sudo schroot -c arm32
-
+  wget -O - "https://forum.armbian.com/applications/core/interface/file/attachment.php?id=8580&key=61d35a8d10b75dcd080f73ff061a9d2a" 
+  wget -O - "https://forum.armbian.com/applications/core/interface/file/attachment.php?id=8581&key=03b1d32e8620bbe0620cb99cb547746f" 
+  wget -O - "https://forum.armbian.com/applications/core/interface/file/attachment.php?id=8582&key=dca9fbfc0a49a7e31d09783902e20204" 
+  wget -O - "https://forum.armbian.com/applications/core/interface/file/attachment.php?id=8583&key=f61a84ed20203e142f5a63667c7625a5" 
+} > arm-linux-gnueabihf.7z
 
 
