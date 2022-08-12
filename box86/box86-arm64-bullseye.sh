1db9ecfc6a735431db5165d7b7c65195ec0b7274
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2022-08-12 18:48:26 +0000

    Update box86-arm64-bullseye.sh

--- box86/box86-arm64-bullseye.sh
+++ box86/box86-arm64-bullseye.sh
@@ -2,20 +2,20 @@
 
 # See: https://medium.com/@safiuddinkhan/how-to-install-box86-box64-and-wine-on-raspberry-pi-os-bullseye-64-bit-c88028052f8c
 
-sudo apt-get -y install build-essential mono-runtime git cmake
+sudo apt-get -y install build-essential mono-runtime git cmake cabextract gcc-arm-linux-gnueabihf libc6-dev-armhf-cross zenity
 
 sudo dpkg --add-architecture armhf
 sudo apt-get -y install gcc-arm-linux-gnueabihf
 
+sudo apt-get -y install libegl-mesa0:armhf libgdm1:armhf libgl1-mesa-dri:armhf libglapi-mesa:armhf \
+  libgles2-mesa:armhf libglu1-mesa:armhf libglx-mesa0:armhf mesa-va-drivers:armhf mesa-vdpau-drivers:armhf \
+  mesa-vulkan-drivers:armhf libsdl1.2debian:armhf libsdl2-2.0-0:armhf libudev1:armhf
+  
 cd ~
 git clone https://github.com/ptitSeb/box86 && cd box86
 mkdir build; cd build; cmake .. -DRPI4ARM64=1 -DCMAKE_BUILD_TYPE=RelWithDebInfo; make -j4
 sudo make install
 sudo systemctl restart systemd-binfmt
-
-sudo apt-get -y install libegl-mesa0:armhf libgdm1:armhf libgl1-mesa-dri:armhf libglapi-mesa:armhf \
-  libgles2-mesa:armhf libglu1-mesa:armhf libglx-mesa0:armhf mesa-va-drivers:armhf mesa-vdpau-drivers:armhf \
-  mesa-vulkan-drivers:armhf libsdl1.2debian:armhf libsdl2-2.0-0:armhf libudev1:armhf
 
 # Wine
 sudo apt-get -y install libasound2:armhf libc6:armhf libglib2.0-0:armhf libgphoto2-6:armhf libgphoto2-port12:armhf libgstreamer-plugins-base1.0-0:armhf \
@@ -25,9 +25,8 @@ sudo apt-get -y install libasound2:armhf libc6:armhf libglib2.0-0:armhf libgphot
  libgstreamer1.0-0:armhf libldap-2.4-2:armhf libopenal1:armhf libpcap0.8:armhf libpulse0:armhf libsane1:armhf \
  libudev1:armhf libunwind8:armhf libusb-1.0-0:armhf libvkd3d1:armhf libx11-6:armhf libxext6:armhf ocl-icd-libopencl1:armhf \
  libasound2-plugins:armhf libncurses6:armhf libmpeg2-4:armhf libmpeg2encpp-2.1-0:armhf libtinfo6:armhf \
- libmpg123-0:armhf libtinfo5:armhf cabextract:armhf libxslt1.1:armhf zenity:armhf
+ libmpg123-0:armhf libtinfo5:armhf libxslt1.1:armhf
 
-cd
 cd ~/Downloads
 wget https://dl.winehq.org/wine-builds/debian/dists/bullseye/main/binary-i386/wine-stable-i386_6.0.4~bullseye-1_i386.deb
 wget https://dl.winehq.org/wine-builds/debian/dists/bullseye/main/binary-i386/wine-stable_6.0.4~bullseye-1_i386.deb
@@ -35,8 +34,7 @@ dpkg-deb -xv wine-stable-i386_6.0.4~bullseye-1_i386.deb wine-installer
 dpkg-deb -xv wine-stable_6.0.4~bullseye-1_i386.deb wine-installer
 mv ~/Downloads/wine-installer/opt/wine* ~/wine
 rm -rf wine-installer
-cd
-
+cd ~
 
 sudo ln -s ~/wine/bin/wine /usr/local/bin/wine
 sudo ln -s ~/wine/bin/wineboot /usr/local/bin/wineboot
