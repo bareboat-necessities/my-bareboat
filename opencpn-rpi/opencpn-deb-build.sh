fd9a8e23aa3e421d6ffa6effb6182f79bf3e3b40
mgrouch <mgrushinskiy@gmail.com>
2022-09-02 15:36:23 -0400

    fix

--- opencpn-rpi/opencpn-deb-build.sh
+++ opencpn-rpi/opencpn-deb-build.sh
@@ -7,6 +7,6 @@ sudo apt install devscripts equivs
 dget -u -x http://deb.debian.org/debian/pool/main/o/opencpn/opencpn_5.6.2+dfsg-2.dsc
 cd opencpn-5.6.2+dfsg
 sudo mk-build-deps -i -r
-sudo rm *.buildinfo *.changes
+sudo rm ./*.buildinfo ./*.changes
 debuild -us -uc
 
