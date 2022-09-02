ce04520e0324bf356501b51827f78684a61e2b7e
mgrouch <mgrushinskiy@gmail.com>
2022-09-02 12:18:18 -0400

    deb

---
+++ opencpn-rpi/opencpn-deb-build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -e
+
+mkdir  opencpn-5.6.2
+cd  opencpn-5.6.2
+sudo apt update
+sudo apt install devscripts equivs
+dget -u -x http://deb.debian.org/debian/pool/main/o/opencpn/opencpn_5.6.2+dfsg-2.dsc
+cd opencpn-5.6.2+dfsg
+sudo mk-build-deps -i -r
+debuild -us -uc
+
