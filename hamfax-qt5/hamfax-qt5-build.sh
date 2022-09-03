94f95d4db8fb57c74ab824311b8f9c00db67c14f
mgrouch <mgrushinskiy@gmail.com>
2022-09-03 11:43:39 -0400

    upd

--- hamfax-qt5/hamfax-qt5-build.sh
+++ hamfax-qt5/hamfax-qt5-build.sh
@@ -2,7 +2,7 @@
 
 sudo apt-get -y install qtbase5-dev qtbase5-dev-tools qttools5-dev-tools
 
-sudo cp /usr/bin/lrelease /usr/lib/qt5/bin/
+#sudo cp /usr/bin/lrelease /usr/lib/qt5/bin/
 
 git clone https://github.com/sergioisidoro/hamfax-qt5
 cd hamfax-qt5/
