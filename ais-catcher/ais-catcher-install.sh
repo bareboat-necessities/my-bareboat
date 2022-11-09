4778abc7347ccb02a546e43d081421c01290d8ec
mgrouch <mgrushinskiy@gmail.com>
2022-11-09 13:37:02 -0500

    cleanup

--- ais-catcher/ais-catcher-install.sh
+++ ais-catcher/ais-catcher-install.sh
@@ -3,7 +3,7 @@
 # See: https://github.com/jvde-github/AIS-catcher
 
 sudo apt-get install git make gcc g++ cmake pkg-config -y librtlsdr-dev libairspy-dev libairspyhf-dev \
-  libhackrf-dev libsoapysdr-dev libzmq3-dev
+  libhackrf-dev libsoapysdr-dev libzmq3-dev libcurl4-openssl-dev zlib1g-dev
 git clone https://github.com/jvde-github/AIS-catcher.git
 cd AIS-catcher
 mkdir build
@@ -11,4 +11,8 @@ cd build
 cmake ..
 make -j 4
 sudo make install
+
+# For runtime
+sudo apt-get install -y librtlsdr libairspy libairspyhf \
+  libhackrf libsoapysdr libzmq3 libcurl4-openssl zlib1g
 
