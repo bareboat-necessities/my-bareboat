5d98522d1d022059a57a6737bac50fe48de91d47
Mikhail Grushinskiy <mgrushinskiy@gmail.com>
2025-02-27 19:53:49 -0500

    upd

--- m5stack-tools/bbn-flash-m5-hub.sh
+++ m5stack-tools/bbn-flash-m5-hub.sh
@@ -31,7 +31,7 @@ fi
 
 mkdir /tmp/bbn_esp32_sensors_hub && cd /tmp/bbn_esp32_sensors_hub
 
-wget https://github.com/bareboat-necessities/bbn_esp32_sensors_hub/releases/download/v1.0.14/bbn_esp32_sensors_hub_bin-2025-02-27.zip
+wget https://github.com/bareboat-necessities/bbn_esp32_sensors_hub/releases/download/v1.0.15/bbn_esp32_sensors_hub_bin-2025-02-27.zip
 unzip bbn_esp32_sensors_hub_bin-2025-02-27.zip
 /srv/esphome/bin/esptool.py --chip esp32s3 \
  --port "$parameterP" --baud 921600 \
