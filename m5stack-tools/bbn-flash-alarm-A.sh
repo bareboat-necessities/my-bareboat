73fce360f1762adf11d179d1f14d1b9a7aee4391
Mikhail Grushinskiy <mgrushinskiy@gmail.com>
2025-03-17 17:31:31 -0400

    upd

---
+++ m5stack-tools/bbn-flash-alarm-A.sh
@@ -0,0 +1,42 @@
+#!/bin/bash -e
+
+# See: https://github.com/bareboat-necessities/bbn_alarm_A
+
+usage() {
+   echo "Usage: $0 -p ttyPort"
+   echo -e "\t-p ttyPort of esp32 device ex: /dev/ttyACM1"
+   exit 1
+}
+
+while getopts "p:" opt
+do
+   case "$opt" in
+      p ) parameterP="$OPTARG" ;;
+      ? ) usage ;;
+   esac
+done
+
+# Print helpFunction in case parameters are empty
+if [ -z "$parameterP" ]
+then
+   echo "Option -p value is required";
+   usage
+fi
+
+m_dir=$(pwd)
+
+if [ -d /tmp/bbn_alarm_A ]; then
+  rm -rf /tmp/bbn_alarm_A/
+fi
+
+mkdir /tmp/bbn_alarm_A && cd /tmp/bbn_alarm_A
+
+wget https://github.com/bareboat-necessities/bbn_alarm_A/releases/download/vTest/bbn_alarm_A_bin_firmware-2025-03-17.zip
+unzip bbn_alarm_A_bin_firmware-2025-03-17.zip
+/srv/esphome/bin/esptool.py --chip esp32s3 \
+ --port "$parameterP" --baud 921600 \
+ --before default_reset --after hard_reset write_flash \
+ 0x0 bbn_alarm_A.merged.bin
+
+cd "$m_dir"
+rm -rf /tmp/bbn_alarm_A/
