e65eb7e2b6dfb785828e61bed05c74d13571d54a
Mikhail Grushinskiy <mgrushinskiy@gmail.com>
2025-03-19 00:17:02 -0400

    upd

--- m5stack-tools/bbn-flash-alarms-A.sh
+++ m5stack-tools/bbn-flash-alarms-A.sh
@@ -31,7 +31,7 @@ fi
 
 mkdir /tmp/bbn_alarms_A && cd /tmp/bbn_alarms_A
 
-wget https://github.com/bareboat-necessities/bbn_alarms_A/releases/download/v1.0.1/bbn_alarms_A_bin_firmware-2025-03-19.zip
+wget https://github.com/bareboat-necessities/bbn_alarms_A/releases/download/v1.0.2/bbn_alarms_A_bin_firmware-2025-03-19.zip
 unzip bbn_alarms_A_bin_firmware-2025-03-19.zip
 /srv/esphome/bin/esptool.py --chip esp32s3 \
  --port "$parameterP" --baud 921600 \
