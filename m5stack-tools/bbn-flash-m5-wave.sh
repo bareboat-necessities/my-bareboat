307df168a189c30ae1bc2c130f7f2d231275ad39
Mikhail Grushinskiy <mgrushinskiy@gmail.com>
2025-05-03 11:43:08 -0400

    upd

--- m5stack-tools/bbn-flash-m5-wave.sh
+++ m5stack-tools/bbn-flash-m5-wave.sh
@@ -31,8 +31,8 @@ fi
 
 mkdir /tmp/bbn-flash-m5-wave && cd /tmp/bbn-flash-m5-wave
 
-wget https://github.com/bareboat-necessities/bbn-wave-period-esp32/releases/download/v1.0.3/bbn_wave_freq_m5atomS3_bin-2025-05-02.zip
-unzip bbn_wave_freq_m5atomS3_bin-2025-05-02.zip
+wget https://github.com/bareboat-necessities/bbn-wave-period-esp32/releases/download/v1.0.6/bbn_wave_freq_m5atomS3_bin-2025-05-03.zip
+unzip bbn_wave_freq_m5atomS3_bin-2025-05-03.zip
 /srv/esphome/bin/esptool.py --chip esp32s3 \
  --port "$parameterP" --baud 921600 \
  --before default_reset --after hard_reset write_flash \
